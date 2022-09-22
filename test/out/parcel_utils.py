fd473a28b5ab382df37d8a10d1d104e69c40282e
Santiago Weight <santiago.weight@lmns.com>
2022-09-22 12:45:14 +0100

    update parcel_utils

--- test/out/parcel_utils.py
+++ test/out/parcel_utils.py
@@ -1,35 +1,25 @@
 from typing import *
 
+
 def encode_bool(bool):
-  return "true" if bool else "false"
+    return "true" if bool else "false"
+
 
 # TODO type hints
 def decode_list(json, decode_elem):
-    res = list()
-    for e in json:
-        res.append(decode_elem(e))
-    return res
+    return [decode_elem(e) for e in json]
 
 
 def encode_list(data, encode_elem):
-    res = list()
-    for e in data:
-        res.append(encode_elem(e))
-    return res
+    return [encode_elem(e) for e in data]
 
 
 def encode_map(data, key_to_str, encode_elem):
-    res = dict()
-    for k, v in data.items():
-        res[key_to_str(k)] = encode_elem(v)
-    return res
+    return {key_to_str(k): encode_elem(v) for k, v in data.items()}
 
 
 def encode_map_as_list(data, encode_key, encode_elem):
-    res = list()
-    for k, v in data.items():
-        res.append([encode_key(k), encode_elem(v)])
-    return res
+    return [[encode_key(k), encode_elem(v)] for k, v in data.items()]
 
 
 def encode_optional(data, encode_elem):
@@ -40,14 +30,8 @@ def encode_optional(data, encode_elem):
 
 
 def decode_map(data, key_from_str, decode_elem):
-    res = dict()
-    for k, v in data.items():
-        res[key_from_str(k)] = decode_elem(v)
-    return res
+    return {key_from_str(k): decode_elem(v) for k, v in data.items()}
 
 
 def decode_map_from_list(data, decode_key, decode_elem):
-    res = dict()
-    for k, v in data:
-        res[decode_key(k)] = decode_elem(v)
-    return res
+    return {decode_key(k): decode_elem(v) for k, v in data}
